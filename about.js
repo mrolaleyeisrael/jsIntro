5b7a343c2d9b77bbe5cc66e9d930757611fb13c7
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2022-06-20 20:27:09 +0100

    submitted

---
+++ about.js
@@ -0,0 +1,5 @@
+var myName = "Israel Olaleye";
+var height = "1.86 metres";
+var country = "Nigeria";
+alert("My name is " + myName + ", I am " + height + " tall, "+ "I am from " + country);
+console.log("My name is " + myName + ", I am " + height + " tall, "+ "I am from " + country);
